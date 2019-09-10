230d379ab1f0e4e10b8aec98b9961e6b94627c40
Nian <43694725+NigelNN@users.noreply.github.com>
2019-09-10 14:21:17 +0800

    Create 328OddEvenLinkedList.java
    LeetCode#328 奇偶链表

---
+++ Leetcode/LinearStructure/328OddEvenLinkedList.java
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        // head 为奇链表头结点，o 为奇链表尾节点
+        ListNode o = head;
+        // p 为偶链表头结点
+        ListNode p = head.next;
+        // e 为偶链表尾节点
+        ListNode e = p;
+        while (o.next != null && e.next != null) {
+            o.next = e.next;
+            o = o.next;
+            e.next = o.next;
+            e = e.next;
+        }
+        o.next = p;
+        return head;
+    }
+}
+
+//解决链表问题最好的办法是在脑中或者纸上把链表画出来
